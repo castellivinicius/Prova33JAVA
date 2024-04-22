de7f47728dac531ac67f5822f34f48ab2d582699
Vinicius Sampaio Castelli de Deus <ra2257075@CTIBAURU.FEB.UNESP.BR>
2024-04-22 14:38:01 -0300

    first commit

---
+++ BD/banco33.sql
@@ -0,0 +1,28 @@
+CREATE TABLE estado(
+	id_estado serial NOT NULL PRIMARY KEY,
+	nome varchar(200),
+	lider varchar(200),
+	PIB varchar(200),
+	classe varchar(200)
+);
+
+CREATE TABLE tratado(
+	id_tratado serial NOT NULL PRIMARY KEY,
+	id_signatarioUm serial NOT NULL,
+	id_signatarioDois serial NOT NULL,
+	tipo varchar(200),
+	dataAssinatura date,
+	FOREIGN KEY (id_signatarioUm) REFERENCES estado(id_estado),
+	FOREIGN KEY (id_signatarioDois) REFERENCES estado(id_estado)
+);
+
+CREATE TABLE guerra(
+	id_guerra serial NOT NULL PRIMARY KEY,
+	id_atacante serial NOT NULL,
+	id_defensor serial NOT NULL,
+	tipo varchar(200),
+	dataInicio date NOT NULL,
+	dataFim date,
+	FOREIGN KEY (id_atacante) REFERENCES estado(id_estado),
+	FOREIGN KEY (id_defensor) REFERENCES estado(id_estado)
+);
---
+++ src/prova33_1bi/Prova33_1bi.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prova33_1bi;
+
+/**
+ *
+ * @author ra2257075
+ */
+public class Prova33_1bi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
